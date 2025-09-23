5fc276c35f17c43ca1b408b86b633a6748f0c728
salkem2 <232410149@smkn1padaherang.sch.id>
2025-09-23 14:42:08 +0700

    first

---
+++ app/Models/kategori.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class kategori extends Model
+{
+    protected $fillable = ['nama_kategori'];
+    public function barangs(): HasMany
+    {
+        return $this->hasMany(Barang::class, 'kategori_id');
+    }
+}
---
+++ resources/views/welcome-partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer id="kontak" class="bg-primary text-white text-center py-4 mt-auto">
+  <div class="container">
+    <p class="mb-0">&copy; {{ date('Y') }} Sistem Inventaris Barang. All Rights Reserved.</p>
+  </div>
+</footer>
